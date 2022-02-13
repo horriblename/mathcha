2ab78d62b005845004bb531817a57dce39dae815
Ching Pei Yang <badnam3o.0@gmail.com>
2022-02-13 15:41:24 +0100

    Auto enter newly created command container in Editor

--- mathrender/editor.go
+++ mathrender/editor.go
@@ -338,9 +338,11 @@ func (e *Editor) InsertCmd(cmd string) {
 	case kind.TakesOneArg():
 		node := &parser.Cmd1ArgExpr{Type: kind, Arg1: new(parser.CompositeExpr)}
 		e.getParent().InsertChildren(idx, node)
+		e.enterContainerFromRight(node)
 	case kind.TakesTwoArg():
 		node := &parser.Cmd2ArgExpr{Type: kind, Arg1: new(parser.CompositeExpr), Arg2: new(parser.CompositeExpr)}
 		e.getParent().InsertChildren(idx, node)
+		e.enterContainerFromRight(node)
 	case kind.TakesRawStrArg():
 	case kind.IsVanillaSym():
 		node := &parser.SimpleCmdLit{Type: kind, Source: cmd}
