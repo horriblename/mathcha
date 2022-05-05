506fc98a7061cc4f33bcc9654bc7a691b865518e
Ching Pei Yang <badnam3o.0@gmail.com>
2022-05-05 12:49:48 +0200

    Added sample app main.go

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	// tea "github.com/charmbracelet/bubbletea"
+	// parser "github.com/horriblename/mathcha/latex"
+	// render "github.com/horriblename/mathcha/renderer"
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+	ed "github.com/horriblename/mathcha/editor"
+)
+
+type model struct {
+	editor ed.Editor
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func initialModel() model {
+	return model{*ed.New()}
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+
+		// We handle errors just like any other message
+		// case errMsg:
+		// 	m.err = msg
+		// 	return m, nil
+	}
+
+	m.editor, cmd = m.editor.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"math?\n\n%s\n\n%s",
+		m.editor.View(),
+		"(ctrl+c to quit)",
+	) + "\n"
+}
+
+func main() {
+	var latex string
+	latex = `1-\frac{1}{1-\frac{1}{1-\frac{1}{1-\frac{1}{1-\frac{1}{1-\frac{1}{1-\frac{1}{1-\frac{1}{1-\frac{1}{1-\frac{1}{2}}}}}}}}}}`
+	latex = `E = \frac\underline{12}12 mv2`
+	// latex = `g(x) = \left(\frac{12}{13}\right)`
+	latex = `xyz = \text{this is a text}abc-{}+1`
+
+	latex = `f(x) = \frac{1}{\sigma\sqrt{2\pi}}\exp\left(-\frac{1}{2}\left(\frac{x-\mu}{\sigma}\right)\right)`
+	latex = "f(x)"
+	// latex = `E = mc^2`
+	e := initialModel()
+	e.editor.Read(latex)
+
+	// e := render.InitialModel(latex)
+
+	p := tea.NewProgram(e)
+	if err := p.Start(); err != nil {
+		// log error
+	}
+}
