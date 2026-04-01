7f47709ceefe1b830e8cca40e1befcaa746a43ab
Ching Pei Yang <badnam3o.0@gmail.com>
2026-04-02 01:20:18 +0200

    plugin: groundwork for updating extmarks

--- extras/nvim/lua/mathcha.lua
+++ extras/nvim/lua/mathcha.lua
@@ -2,8 +2,16 @@ local M = {}
 ---@type table<integer, State>
 M._states = {}
 
+---@alias NodeID string
+
+---@class NodeBinding
+---@field ext_ids integer[]
+---@field running vim.SystemObj?
+---@field needs_rerun {start: integer, end_: integer}? used to indicate the running job is outdated and should be rerun
+
 ---@class State
 ---@field buf integer
+---@field jobs table<NodeID, NodeBinding>
 local State = {}
 
 local ns_id = vim.api.nvim_create_namespace("mathcha")
@@ -30,7 +38,7 @@ end
 ---@return State?
 function State.new(bufnr)
 	vim.api.nvim_buf_clear_namespace(bufnr, ns_id, 0, -1)
-	local state = setmetatable({ buf = bufnr }, { __index = State })
+	local state = setmetatable({ buf = bufnr, jobs = {} }, { __index = State })
 
 	local query = vim.treesitter.query.parse('markdown_inline', [[ (latex_block) @latex ]])
 	local md_inline_tree = vim.treesitter.get_parser(bufnr, "markdown")
@@ -41,20 +49,20 @@ function State.new(bufnr)
 	end
 
 	md_inline_tree:parse(
-		true, -- TODO: parse only visible region maybe
+		false, -- TODO: parse only visible region maybe
 		function(err, trees)
 			if err ~= nil or trees == nil then
-				vim.notify(("failed to parse buffer %d: %s"):format(bufnr, err))
+				vim.notify(("failed to parse buffer %d: %s"):format(bufnr, err), vim.log.levels.ERROR)
 				return
 			end
 
-			for i, tree in pairs(trees) do
+			for _, tree in pairs(trees) do
 				for _, match, _ in query:iter_matches(tree:root(), bufnr, 0, -1) do
 					for _, nodes in pairs(match) do
 						for _, node in ipairs(nodes) do
 							local start_row, start_col, end_row, end_col = node:range()
 							-- FIXME: start_row+1 to skip $$ but will probably break something down the line
-							state:create_conceal(start_row + 1, end_row)
+							state:update_conceal(node:id(), start_row + 1, end_row)
 						end
 					end
 				end
@@ -62,18 +70,27 @@ function State.new(bufnr)
 		end
 	)
 
-
 	return state
 end
 
-function State:create_conceal(start_row, end_row)
+---@param node_id NodeID
+---@param start_row integer
+---@param end_row integer
+function State:update_conceal(node_id, start_row, end_row)
 	assert(start_row < end_row,
 		string.format(
 			"invariant (start_row < end_row) violated: start_row=%d end_row=%d",
 			start_row, end_row
 		))
 
-	vim.system({ 'mathcha', '-render' }, {
+	if self.jobs[node_id] and self.jobs[node_id].running then
+		self.jobs[node_id].needs_rerun = { start = start_row, end_ = end_row }
+		return
+	end
+
+	self.jobs[node_id] = { ext_ids = {} }
+
+	self.jobs[node_id].running = vim.system({ './mathcha', '-render' }, {
 		stdin = vim.api.nvim_buf_get_lines(self.buf, start_row, end_row, false)
 	}, function(obj)
 		-- TODO: check exit code
@@ -82,22 +99,28 @@ function State:create_conceal(start_row, end_row)
 		end
 		vim.schedule(function()
 			local lines = enumerate(vim.gsplit(obj.stdout, '\n', { plain = true }))
+
+			for _, x in ipairs(self.jobs[node_id].ext_ids) do
+				vim.api.nvim_buf_del_extmark(self.buf, ns_id, x)
+			end
+
+			local extmarks = {}
 			---@type integer?
 			local last_replaced_row
 			for i, line in lines do
 				local row = start_row + i
 				last_replaced_row = row
 				if row ~= end_row then
-					vim.api.nvim_buf_set_extmark(self.buf, ns_id, row, 0, {
-						-- invalidate = true,
+					table.insert(extmarks, vim.api.nvim_buf_set_extmark(self.buf, ns_id, row, 0, {
+						invalidate = true,
 						virt_text = { { line } },
 						virt_text_pos = "overlay",
 						virt_text_hide = true,
 						conceal = "",
 						-- why -1 no worky??
-						end_col = math.huge,
-						strict = true,
-					})
+						end_col = 999999,
+						strict = false,
+					}))
 				else
 					-- squash the rest into one big virt_lines
 					---@type string[][][]
@@ -106,18 +129,17 @@ function State:create_conceal(start_row, end_row)
 						table.insert(rest, { { l } })
 					end
 
-					vim.api.nvim_buf_set_extmark(self.buf, ns_id, row, 0, {
-						-- invalidate = true,
+					table.insert(extmarks, vim.api.nvim_buf_set_extmark(self.buf, ns_id, row, 0, {
+						invalidate = true,
 						virt_lines = rest,
 						virt_text_pos = "overlay",
 						virt_lines_above = true,
 						virt_text_hide = true,
 						conceal = "",
 
-						-- TODO: probably there's a better way
-						end_col = 9999,
+						end_col = 999999,
 						strict = false,
-					})
+					}))
 				end
 			end
 
@@ -126,7 +148,7 @@ function State:create_conceal(start_row, end_row)
 					vim.api.nvim_buf_set_extmark(self.buf, ns_id, i, 0, {
 						virt_text_hide = true,
 						conceal = "",
-						end_col = 9999,
+						end_col = 999999,
 						strict = false,
 					})
 				end
@@ -139,6 +161,13 @@ function State:create_conceal(start_row, end_row)
 						strict = false,
 					})
 				end
+			end
+
+			self.jobs[node_id].running = nil
+			local params = self.jobs[node_id].needs_rerun
+			if params then
+				self.jobs[node_id].needs_rerun = nil
+				self:update_conceal(node_id, params.start, params.end_)
 			end
 		end)
 	end)
